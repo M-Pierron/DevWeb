a078ffbc8ada6c0a6903bef27ac684e501d72897
Damien.M <133344364+MouchardDamien@users.noreply.github.com>
2025-03-29 18:43:59 +0100

    Update Visualization.jsx

--- src/pages/Visualization.jsx
+++ src/pages/Visualization.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
 import ToolsList from "../components/Visualization/toolsList"
 import DashBoardFrame from "../components/Visualization/dashBoardFrame"
+import "../App.css"
 
-const Visualization = () => {
+function Visualization() {
   return (
     <>
         <div className='h-dvh flex justify-between m-5'>
