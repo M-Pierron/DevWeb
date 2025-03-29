d9b4a81c6a2fb95ccc7fd052399a6890a4da475a
Damien.M <133344364+MouchardDamien@users.noreply.github.com>
2025-03-29 18:48:25 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { BrowserRouter, createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router"
+import { BrowserRouter, createBrowserRouter, createRoutesFromElements, Route, Routes, Navigate, useNavigate, RouterProvider } from "react-router"
+import Start from "./pages/Start"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
 import Main from "./components/main"
 import Home from "./pages/Home"
 import Header from "./pages/Header"
 import Visualization from "./pages/Visualization"
+import './App.css'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Header />}>
-      <Route index element={<Home />} />
-      <Route path="visualization" element={<Visualization />} />
-    </Route>
+    <>
+      <Route path="/" element={<Start />} />
+      <Route path="/Accueil" element={<Home />} />
+      <Route path="/Accueil/Connexion" element={<SignIn />} />
+      <Route path="/Accueil/Inscription" element={<SignUp />} />
+      <Route path="/Accueil/Visualisation" element={<Visualization />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </>
+
   )
 )
 
@@ -20,5 +29,6 @@ function App() {
     </>
   )
 }
+
 
 export default App
