7c61ef1a70c74a35176248ff6fa17d4eff089c01
Damien.M <133344364+MouchardDamien@users.noreply.github.com>
2025-03-29 18:50:58 +0100

    Update nav.jsx

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,17 +1,20 @@
-import { Outlet, Link } from "react-router";
+import { Outlet, Link } from "react-router"
 
-const nav = () => {
+function Nav({name}) {
   return (
     <>
-        <div className="flex items-center justify-between p-10 lg:flex-row bg-indigo-700">
-            <div>CyHouse</div>
-            <div>
-                <Link to="/">Home</Link>
-                <Link to="/visualization">Vizualization</Link>
+        <div className="nav">
+            <div className="nav_title">{name}</div>
+            <div className="navElements">
+                <Link className="navElement" to="/">Accueil </Link>
+                <Link className="navElement" to="/Accueil/Connexion">Connexion </Link>
+                <Link className="navElement" to="/Accueil/Inscription">Inscription </Link>
+                <Link className="navElement" to="/Accueil/Visualisation">Visualisation </Link>
+                <Link className="navElement" to="/Accueil/À Propos">À Propos </Link>
             </div>
         </div>
     </>
   )
 }
 
-export default nav
+export default Nav
