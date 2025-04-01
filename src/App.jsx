39a13877a931f9ce71631327f9c76f26de9ef24c
Damien.M <133344364+MouchardDamien@users.noreply.github.com>
2025-04-01 19:07:21 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { BrowserRouter, createBrowserRouter, createRoutesFromElements, Route, Routes, Navigate, useNavigate, RouterProvider } from "react-router"
 import Start from "./pages/Start"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import Main from "./components/main"
+import Sign from "./pages/Sign"
 import Home from "./pages/Home"
-import Header from "./pages/Header"
+import About from "./pages/About"
 import Visualization from "./pages/Visualization"
 import './App.css'
 
@@ -13,8 +11,8 @@ const router = createBrowserRouter(
     <>
       <Route path="/" element={<Start />} />
       <Route path="/Accueil" element={<Home />} />
-      <Route path="/Accueil/Connexion" element={<SignIn />} />
-      <Route path="/Accueil/Inscription" element={<SignUp />} />
+      <Route path="/Accueil/Connexion&Inscription" element={<Sign />} />
+      <Route path="/Accueil/À Propos" element={<About />} />
       <Route path="/Accueil/Visualisation" element={<Visualization />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </>
