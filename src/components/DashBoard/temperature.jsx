ffa89dcafd592bc8b5da7c77a84ed211c9b3d58e
M-Pierron <130355886+M-Pierron@users.noreply.github.com>
2025-04-08 10:33:53 +0200

    Fusion branches

--- src/components/DashBoard/temperature.jsx
+++ src/components/DashBoard/temperature.jsx
@@ -14,18 +14,18 @@ const temperature = () => {
         <div className='flex flex-col w-full h-full p-4'>
           <span className='self-center'>Température actuelle</span>
           {/* <!-- Gauge Component --> */}
-          <div class="relative size-full">
-              <svg class="rotate-[135deg] size-full" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg">
+          <div className="relative size-full">
+              <svg className="rotate-[135deg] size-full" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg">
                   {/* <!-- Background Circle (Gauge) --> */}
-                  <circle cx="18" cy="18" r="16" fill="none" class="stroke-current text-green-200 dark:text-neutral-700" stroke-width="1" stroke-dasharray="75 100" stroke-linecap="round"></circle>
+                  <circle cx="18" cy="18" r="16" fill="none" className="stroke-current text-green-200 dark:text-neutral-700" strokeWidth="1" strokeDasharray="75 100" strokeLinecap="round"></circle>
 
                   {/* <!-- Gauge Progress --> */}
-                  <circle cx="18" cy="18" r="16" fill="none" class="stroke-current text-green-500 dark:text-green-500" stroke-width="2" stroke-dasharray="56.25 100" stroke-linecap="round"></circle>
+                  <circle cx="18" cy="18" r="16" fill="none" className="stroke-current text-green-500 dark:text-green-500" strokeWidth="2" strokeDasharray="19 100" strokeLinecap="round"></circle>
               </svg>
 
               {/* <!-- Value Text --> */}
-              <div class="absolute top-1/2 start-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-                  <span class="text-fit font-bold text-green-600 dark:text-green-500">19.0°C</span>
+              <div className="absolute top-1/2 start-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+                  <span className="text-fit font-bold text-green-600 dark:text-green-500">19.0°C</span>
               </div>
           </div>
           {/* <!-- End Gauge Component --> */}
@@ -33,13 +33,13 @@ const temperature = () => {
           {/* <!-- Start Slider Component --> */}
           <div className='flex flex-col'>
             <span className='self-center'>Température Cible</span>
-            <div class="p-4">
-              <span class="text-sm"></span>
-              <span class="text-sm">{targetTemperatureValue}°C</span>
-              <input class="w-full accent-indigo-600" type="range" name="" min="0" max="90" value={targetTemperatureValue} onChange={onTargetTemperatureSliderChange}/>
-              <div class="-mt-2 flex w-full justify-between">
-                <span class="text-sm text-black font-bold">0</span>
-                <span class="text-sm text-black font-bold">90</span>
+            <div className="p-4">
+              <span className="text-sm"></span>
+              <span className="text-sm">{targetTemperatureValue}°C</span>
+              <input className="w-full accent-indigo-600" type="range" name="" min="0" max="90" value={targetTemperatureValue} onChange={onTargetTemperatureSliderChange}/>
+              <div className="-mt-2 flex w-full justify-between">
+                <span className="text-sm text-black font-bold">0</span>
+                <span className="text-sm text-black font-bold">90</span>
               </div>
             </div>
           </div>
--- src/components/DashBoard/wifi.jsx
+++ src/components/DashBoard/wifi.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const wifi = () => {
   return (
-    <div class="flex justify-evenly items-end w-24 h-12 rounded-lg w-full h-full">
-      <div class="w-full h-[20%] mr-2 bg-green-500 rounded-xl"></div>
-      <div class="w-full h-[40%] mr-2 bg-green-500 rounded-xl"></div>
-      <div class="w-full h-[60%] mr-2 bg-green-500 rounded-xl"></div>
-      <div class="w-full h-[80%] mr-2 bg-green-500 rounded-xl"></div>
-      <div class="w-full h-[100%] bg-gray-300  rounded-xl"></div>
+    <div className="flex justify-evenly items-end w-24 h-12 rounded-lg w-full h-full">
+      <div className="w-full h-[20%] mr-2 bg-green-500 rounded-xl"></div>
+      <div className="w-full h-[40%] mr-2 bg-green-500 rounded-xl"></div>
+      <div className="w-full h-[60%] mr-2 bg-green-500 rounded-xl"></div>
+      <div className="w-full h-[80%] mr-2 bg-green-500 rounded-xl"></div>
+      <div className="w-full h-[100%] bg-gray-300  rounded-xl"></div>
     </div>
   )
 }
