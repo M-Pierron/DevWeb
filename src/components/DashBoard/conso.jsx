6c3ee2862c4dfd577414ef026b81c02fa22de639
WirbelWind03 <dylan19@gmail.com>
2025-04-09 00:05:32 +0200

    Removed header to put it in frame.jsx

--- src/components/DashBoard/conso.jsx
+++ src/components/DashBoard/conso.jsx
@@ -10,12 +10,11 @@ import {
   
 const Conso = ({ data }) => {
     if (!data || data.length === 0) {
-      return <div className="text-red-500">⚠️ Aucune donnée de consommation disponible pour cet objet.</div>;
+      return <div className="text-red-500 self-center">⚠️ Aucune donnée de consommation disponible pour cet objet.</div>;
     }
   
     return (
       <div className="w-full h-64 mt-4">
-        <h3 className="text-lg font-bold mb-2">📈 Consommation récente</h3>
         <ResponsiveContainer width="100%" height="100%">
           <LineChart data={data}>
             <CartesianGrid strokeDasharray="3 3" />
