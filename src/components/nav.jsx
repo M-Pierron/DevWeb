cbd0ad6a708029e099dc1f2862c20ac28cafd34c
WirbelWind03 <dylan19@gmail.com>
2025-04-08 13:44:01 +0200

    Old nav

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,49 +1,35 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Logo from "./logo.jsx";
 import { useAuth } from "../context/AuthContext";
-
-import DropDownItem from "./Dropdown/dropDownItem"
 
 function Nav({ name }) {
   const { isConnected, logout } = useAuth();
-  const [isUserProfilDropDownOpened, setIsUserProfilDropDownOpened] = useState(false);
   
   const handleLogout = () => {
     logout();
   };
 
   return (
-    <nav className="flex items-center justify-between bg-[#3c5497] w-full h-[5vw]">
-      <img className="h-full ml-[4vw]" src="/src/assets/logoCYhouse.png" alt="Logo de CY-House."/>
-      <div className="flex items-center gap-[4vw] h-full font-bold">
-        <Link className="hover:scale-125" to="/Accueil">Accueil</Link>
+    <div className="nav">
+      <div className="nav_title">{name}</div>
+      <div className="navElements">
+        <Link className="navElement" to="/Accueil">Accueil</Link>
         
         {!isConnected ? (
-          <Link className="hover:scale-125" to="/Accueil/Connexion&Inscription">Connexion/Inscription</Link>
+          <Link className="navElement" to="/Accueil/Connexion&Inscription">Connexion/Inscription</Link>
         ) : (
           <>
-            <Link className="hover:scale-125" to="/Accueil/Profil">Profil</Link>
-            <Link className="hover:scale-125" to="/Accueil/Visualisation">Visualisation</Link>
-            <Link className="hover:scale-125" to="/" onClick={handleLogout}>Se déconnecter</Link>
+            <Link className="navElement" to="/Accueil/Profil">Profil</Link>
+            <Link className="navElement" to="/Accueil/Visualisation">Visualisation</Link>
+            <Link className="navElement" to="/" onClick={handleLogout}>Se déconnecter</Link>
           </>
         )}
         
-        <Link className="hover:scale-125" to="/Accueil/À Propos">À Propos</Link>
-        
+        <Link className="navElement" to="/Accueil/À Propos">À Propos</Link>
+        <Logo className="logo_nav" />
       </div>
-      <div className="relative h-[60%] mr-[6vw]" onClick={() => setIsUserProfilDropDownOpened((prev) => !prev)}>
-        <label className="size-full">
-          <img className="w-full h-full" src="/src/assets/userProfil.svg"/>
-        </label>
-        {isUserProfilDropDownOpened && <div className="flex flex-col absolute bg-black h-fit w-full">
-          <DropDownItem className={"text-white"} itemName={"Profil"}/>
-          <span>Test</span>
-          <span>Test</span>
-          <span>Test</span>
-          <span>Test</span>
-        </div>}
-      </div>
-    </nav>
+    </div>
   );
 }
 
