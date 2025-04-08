f857a999a23c02cfabe3cad74ddc4495d14d1b22
WirbelWind03 <dylan19@gmail.com>
2025-04-09 00:07:22 +0200

    Added image

--- src/components/Visualization/toolsSearchBar.jsx
+++ src/components/Visualization/toolsSearchBar.jsx
@@ -10,8 +10,11 @@ const toolsSearchBar = ({onToolFilterClick}) => {
   }
 
   return (
-    <div className="flex flex-row h-[5%]">
-        <input type='search' className='w-[90%] bg-white text-black rounded-xl'/>
+    <div className="flex flex-row h-[5%] ">
+        <div className='flex flex-row size-full bg-white text-black rounded-lg'>
+          <img src='/src/assets/magnifyingGlass.svg'/>
+          <input type='search' className='w-[90%] '/>
+        </div>
         <input type="image"  src="/src/assets/filter.svg" className="w-[10%] h-full" onClick={onToolFilterClick}/>
     </div>
   )
