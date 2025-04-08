3a602f3cfdd6102d2697a58b73ad9f22b909906a
WirbelWind03 <dylan19@gmail.com>
2025-04-08 16:00:18 +0200

    Cropped logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,13 @@ function Home() {
   return (
     <div className="flex flex-col w-dvw h-dvh">
       <Nav name={"ACCUEIL"}/>
-      <div className="relative flex flex-row size-full bg-[url(src/assets/wallpaperHome.png)] bg-no-repeat bg-center  ">
+      <div className="flex flex-row size-full bg-[url(src/assets/wallpaperHome.png)] bg-no-repeat bg-center bg-cover">
         <div className="flex flex-col w-full justify-center items-center self-center">
           <img src="src/assets/logoCYHouse.png" className="size-fit"></img>
-          <div className="flex flex-row bg-blue-500 size-fit rounded-full">
-            <span>Dashboard</span>
-            <span>-</span>
+          <span className="font-bold text-black text-xl">CYHOUSE</span>
+          <div className="flex flex-row bg-blue-500 size-fit rounded-full p-4 cursor-pointer">
+            <span>Aller au Dashboard</span>
+            <span></span>
           </div>
         </div>
       </div>
