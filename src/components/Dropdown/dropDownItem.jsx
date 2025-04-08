3a4d04741ab08f721f4bce3daf1822931f6f9b09
WirbelWind03 <dylan19@gmail.com>
2025-04-08 06:07:10 +0200

    Added classname as arg

--- src/components/Dropdown/dropDownItem.jsx
+++ src/components/Dropdown/dropDownItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const dropDownItem = ({itemName, onClick}) => {
+const dropDownItem = ({className, itemName, onClick}) => {
   return (
     <div className="cursor-pointer group" onClick={onClick}>
-        <a className="block p-2 border-transparent border-l-4 group-hover:border-blue-600 group-hover:bg-gray-100">{itemName}</a>
+        <a className={className}>{itemName}</a>
     </div>
   )
 }
