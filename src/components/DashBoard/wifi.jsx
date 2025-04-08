0c779930dec6144e7ade7b327fa5215b662f7e7f
WirbelWind03 <dylan19@gmail.com>
2025-04-09 00:06:36 +0200

    Change bars round corners

--- src/components/DashBoard/wifi.jsx
+++ src/components/DashBoard/wifi.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const wifi = () => {
   return (
     <div className="flex justify-evenly items-end w-24 h-12 rounded-lg w-full h-full">
-      <div className="w-full h-[20%] mr-2 bg-green-500 rounded-xl"></div>
-      <div className="w-full h-[40%] mr-2 bg-green-500 rounded-xl"></div>
-      <div className="w-full h-[60%] mr-2 bg-green-500 rounded-xl"></div>
-      <div className="w-full h-[80%] mr-2 bg-green-500 rounded-xl"></div>
-      <div className="w-full h-[100%] bg-gray-300  rounded-xl"></div>
+      <div className="w-full h-[20%] mr-2 bg-green-500 rounded-lg"></div>
+      <div className="w-full h-[40%] mr-2 bg-green-500 rounded-lg"></div>
+      <div className="w-full h-[60%] mr-2 bg-green-500 rounded-lg"></div>
+      <div className="w-full h-[80%] mr-2 bg-green-500 rounded-lg"></div>
+      <div className="w-full h-[100%] bg-gray-300  rounded-lg"></div>
     </div>
   )
 }
