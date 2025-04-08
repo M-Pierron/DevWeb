25f9f6afd237881e0a9f78b0c907d88c8346d733
WirbelWind03 <dylan19@gmail.com>
2025-04-08 15:42:17 +0200

    Homepage changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,18 @@ import "../App.css"
 
 function Home() {
   return (
-    <div>
+    <div className="flex flex-col w-dvw h-dvh">
       <Nav name={"ACCUEIL"}/>
-      {/* <div className = "home">
-        
-      </div> */}
+      <div className="relative flex flex-row size-full bg-[url(src/assets/wallpaperHome.png)] bg-no-repeat bg-center  ">
+        <div className="flex flex-col w-full justify-center items-center self-center">
+          <img src="src/assets/logoCYHouse.png" className="size-fit"></img>
+          <div className="flex flex-row bg-blue-500 size-fit rounded-full">
+            <span>Dashboard</span>
+            <span>-</span>
+          </div>
+        </div>
+      </div>
+      <div className="bg-red h-[30%]"></div>
     </div>
   )
 }
