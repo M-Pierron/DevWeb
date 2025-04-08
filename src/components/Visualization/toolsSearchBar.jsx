c903ad82d0d8b25c2c8e520e2aaa44ea0328965f
WirbelWind03 <dylan19@gmail.com>
2025-04-09 00:17:27 +0200

    Small changes

--- src/components/Visualization/toolsSearchBar.jsx
+++ src/components/Visualization/toolsSearchBar.jsx
@@ -12,10 +12,10 @@ const toolsSearchBar = ({onToolFilterClick}) => {
   return (
     <div className="flex flex-row h-[5%] ">
         <div className='flex flex-row size-full bg-white text-black rounded-lg'>
-          <img src='/src/assets/magnifyingGlass.svg'/>
-          <input type='search' className='w-[90%] '/>
+          <img src='/src/assets/magnifyingGlass.svg' className='ml-2 mr-2 h-[50%] self-center' />
+          <input type='search' className='w-[90%] outline-none'/>
         </div>
-        <input type="image"  src="/src/assets/filter.svg" className="w-[10%] h-full" onClick={onToolFilterClick}/>
+        <input type="image"  src="/src/assets/filter.svg" className="w-[10%] ml-2 h-full" onClick={onToolFilterClick}/>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,8 @@ function Home() {
   return (
     <div className="flex flex-col w-dvw h-dvh">
       <Nav name={"ACCUEIL"}/>
-      <div className="flex flex-row size-full bg-[url(src/assets/wallpaperHome.png)] bg-no-repeat bg-center bg-cover h-full p-4">
+      
+      <div className="flex flex-row size-full bg-[url(src/assets/wallpaperHome.png)] bg-no-repeat bg-center bg-cover h-[80%] p-4">
         <div className="flex flex-col w-full justify-center items-center self-center">
           <img src="src/assets/logoCYHouse.png" className="size-fit mb-4"></img>
           <span className="font-bold text-black text-6xl mb-4">CYHOUSE</span>
@@ -15,7 +16,8 @@ function Home() {
         </div>
         
       </div>
-      <div className="flex flex-col bg-[#3c5497] w-full h-full border-t-black border-t-2">
+
+      <div className="flex flex-col bg-[#3c5497] w-full h-[20%] border-t-black border-t-2">
         <p className="self-center text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae dui dolor. Cras congue sed eros vitae accumsan. Donec porttitor interdum urna, et dignissim velit consectetur sit amet. Aenean at dapibus tellus, blandit scelerisque quam. Curabitur vehicula magna in sapien pellentesque, in cursus augue accumsan. Suspendisse vel elit sit amet elit placerat vehicula. Vivamus sit amet nibh ex. Sed quis tempor risus. Sed maximus sagittis risus in ornare. Sed interdum, nulla id tempus suscipit, velit ipsum scelerisque turpis, vitae tincidunt nisi nunc sed est.</p>
       </div>
     </div>
