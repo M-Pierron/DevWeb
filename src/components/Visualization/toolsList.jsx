35a8a83f6e4c416ae81da8a5e9e7f4e21f588760
WirbelWind03 <dylan19@gmail.com>
2025-04-09 21:18:43 +0200

    Adding modal from Gestion

--- src/components/Visualization/toolsList.jsx
+++ src/components/Visualization/toolsList.jsx
@@ -3,71 +3,26 @@ import React, { useState, useEffect } from 'react';
 import ToolsFilter from './toolsFilter';
 import ToolsSearchBar from './toolsSearchBar';
 import DeviceItem from "./deviceItem";
+import { Plus } from 'lucide-react';
+import { iotDevices } from "../iotDevices"
+import AddDeviceModal from "./addDeviceModal"
 
 const ToolsList = () => {
-  const iotDevices = [
-    [
-      "Sécurité & Contrôle d'accès", "security",
-      [
-        ["Serrures connectées", "smart_locks"],
-        ["Sonnette vidéo", "video_doorbell"],
-        ["Caméras de sécurité", "security_cameras"],
-        ["Capteurs de mouvement", "motion_sensors"],
-        ["Alarmes connectées", "smart_alarms"]
-      ]
-    ],
-    [
-      "Éclairage & Énergie", "energy",
-      [
-        ["Ampoules connectées", "smart_bulbs"],
-        ["Interrupteurs et variateurs connectés", "smart_switches"],
-        ["Prises intelligentes", "smart_plugs"],
-        ["Multiprises connectées", "smart_power_strips"]
-      ]
-    ],
-    [
-      "Climatisation & Confort", "comfort",
-      [
-        ["Thermostats intelligents", "smart_thermostats"],
-        ["Ventilateurs de plafond connectés", "smart_ceiling_fans"],
-        ["Purificateurs d'air connectés", "smart_air_purifiers"],
-        ["Humidificateurs et déshumidificateurs intelligents", "smart_humidifiers"]
-      ]
-    ],
-    [
-      "Divertissement", "entertainment",
-      [
-        ["Téléviseurs intelligents", "smart_tvs"],
-        ["Enceintes connectées", "smart_speakers"],
-        ["Appareils de streaming", "streaming_devices"],
-        ["Vidéoprojecteurs connectés", "smart_projectors"]
-      ]
-    ],
-    [
-      "Cuisine & Électroménager", "kitchen",
-      [
-        ["Réfrigérateurs intelligents", "smart_fridges"],
-        ["Fours et plaques de cuisson connectés", "smart_ovens"],
-        ["Cafetières connectées", "smart_coffee_machines"],
-        ["Lave-vaisselle intelligents", "smart_dishwashers"]
-      ]
-    ],
-    [
-      "Entretien & Nettoyage", "cleaning",
-      [
-        ["Aspirateurs robots", "robot_vacuums"],
-        ["Machines à laver et sèche-linge connectés", "smart_washers"],
-        ["Systèmes d'arrosage intelligents", "smart_irrigation"]
-      ]
-    ]
-  ];
-
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const [isToolsFilterVisible, setToolsFilterVisibility] = useState(false);
   const [category, setCategory] = useState('');
   const [filteredObjects, setFilteredObjects] = useState([]);
 
   const onToolFilterClick = () => {
     setToolsFilterVisibility(!isToolsFilterVisible);
+  };
+
+  const onAddObjectClick = () => {
+    setIsModalVisible(true);
+  };
+
+  const onCloseModalClick = () => {
+    setIsModalVisible(false);
   };
 
   const handleCategorySelection = async (category) => {
@@ -80,22 +35,32 @@ const ToolsList = () => {
   return (
     <>
       <div className='relative bg-gray-400 rounded-xl p-4 w-[25%] h-dvh'>
+        {/* Le cadre utilisé pour filtrer les appareils, caché */}
         <ToolsFilter tools={iotDevices} isVisible={isToolsFilterVisible} toggleVisibility={onToolFilterClick} />
         <div className='flex flex-col h-full'>
+          {/* La barre de recherche qui contient le button pour afficher le filtre */}
           <ToolsSearchBar onToolFilterClick={onToolFilterClick} />
+          <div 
+            onClick={onAddObjectClick}
+            className="flex flex-row mb-2 mt-4 bg-indigo-600 text-white py-2 rounded-xl hover:bg-indigo-700 cursor-pointer w-[60%] text-center">
+            <Plus className='self-center w-[20%]'/>
+            <span className='self-start w-full'>Ajouter un object</span>
+          </div>
           {/* Tools list */}
-          <div className='h-full mt-4 bg-white text-black'>
+          <div className='flex flex-col h-full bg-white text-black'>
             {/* Afficher les objets filtrés */}
             {filteredObjects.length > 0 ? (
               filteredObjects.map((object) => (
                 <DeviceItem key={object._id} deviceName={object.name} />
               ))
             ) : (
-              <p>Aucun objet trouvé pour cette catégorie.</p>
+              <p className='self-center'>Aucun objet trouvé pour cette catégorie.</p>
             )}
           </div>
         </div>
       </div>
+
+      {isModalVisible && <AddDeviceModal onCancelClick={onCloseModalClick}/>}
     </>
   );
 }
