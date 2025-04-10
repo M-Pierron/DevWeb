857b7057172821035ea908a086b9c33a763c2325
WirbelWind03 <dylan19@gmail.com>
2025-04-11 01:37:05 +0200

    Removed unused files

--- backend/routes/deviceRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { selectDevice, getSelectedDevice } from '../controllers/deviceController.js';
-
-const router = express.Router();
-
-router.post('/select', selectDevice);  // Select a device
-router.get('/selected', getSelectedDevice);  // Get selected device
-
-export default router;
