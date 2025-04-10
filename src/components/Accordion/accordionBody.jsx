757684218a277b73fbcc1e85a870ce6c2cf804c3
WirbelWind03 <dylan19@gmail.com>
2025-04-10 18:52:14 +0200

    Removed unused import

--- src/components/Accordion/accordionBody.jsx
+++ src/components/Accordion/accordionBody.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import AccordionCheckBox from './accordionCheckBox'
-
 const accordionBody = ({style, isOpen, items}) => {
   return (
     <div className={`${style} ${isOpen ? "block" : "hidden"}`}>
