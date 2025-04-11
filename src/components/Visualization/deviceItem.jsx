5ee72308405c25584a71443165634650f64b4250
WirbelWind03 <dylan19@gmail.com>
2025-04-11 16:06:34 +0200

    Changing html for the userDevice object

--- src/components/Visualization/deviceItem.jsx
+++ src/components/Visualization/deviceItem.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
-const deviceItem = ({deviceName, onClick, isSelected}) => {
+const deviceItem = ({userDevice, onClick, isSelected}) => {
   return (
-    <div className={`flex flex-row border-b-2 h-12 ${isSelected ? "bg-gray-100" : "bg-white"} border-gray-400 hover:cursor-pointer font-bold`}>
-        <span className='ml-2 self-center' onClick={onClick}>{deviceName}</span>
+    <div className='flex flex-row h-fit w-full border-b-2 p-4 '>
+      <div className={`flex flex-col w-[50%] ${isSelected ? "bg-gray-100" : "bg-white"} border-gray-400 hover:cursor-pointer`}>
+          <span className='font-bold' onClick={onClick}>{userDevice.name}</span>
+          <span className='text-gray-400'>{userDevice.description}</span>
+
+          <div className='flex flex-row'>
+            <span><strong>Mode : </strong>{userDevice.mode}</span>
+          </div>
+      </div>
+      <div className='flex flex-col w-[50%] items-end'>
+        
+        <span><strong>📶 Wi-Fi : </strong>{userDevice.wifi}</span>
+        <span><strong>🔋 Batterie : </strong>{userDevice.battery}</span>
+
+      </div>
     </div>
+
   )
 }
 
