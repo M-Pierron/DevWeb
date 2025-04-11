eb08912d8a56238b9a98fff7f73d3e873c9c5519
WirbelWind03 <dylan19@gmail.com>
2025-04-11 07:35:00 +0200

    Image are now full path

--- src/components/Accordion/accordionHeader.jsx
+++ src/components/Accordion/accordionHeader.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const accordionHeader = ({style, openStyle, closedStyle, category, clickEvent, isOpen, image}) => {
   return (
     <button className={`${style} ${isOpen ? openStyle : closedStyle}`} onClick={clickEvent}>
-        {image != null && <img src={`/src/assets/${image}`} className='h-full mr-2'/>}
+        {image != null && <img src={image} className='h-full mr-2'/>}
         <div className='w-full h-full flex justify-between'>
           <span className='font-bold self-center'>{category}</span>
           {isOpen ? <span className='self-center'>-</span> : <span className='self-center'>+</span>}
