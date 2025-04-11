cb6070ca70f77dd6e7167e4479538f264d9d086a
WirbelWind03 <dylan19@gmail.com>
2025-04-11 19:42:10 +0200

    Added comments

--- src/components/Visualization/toolsSearchBar.jsx
+++ src/components/Visualization/toolsSearchBar.jsx
@@ -12,11 +12,13 @@ const toolsSearchBar = ({onToolFilterClick}) => {
 
   return (
     <div className="flex flex-row h-[5%] ">
-        <div className='flex flex-row size-full bg-white text-black rounded-lg'>
+        {/* Cadre qui contient la barre de recherche et son icone */}
+        <div className='flex flex-row size-full bg-white text-black rounded-lg border-1 border-black'>
           <Search className='ml-2 mr-2 h-[80%] self-center'/>
           <input type='search' className='w-[90%] outline-none'/>
         </div>
-        <Funnel className="w-[10%] ml-2 h-full text-black bg-gray-500 rounded-lg p-1" onClick={onToolFilterClick}/>
+        {/* Button pour le filtrage */}
+        <Funnel className="w-[10%] ml-2 h-full text-black bg-gray-500 rounded-lg p-1 border-1 border-black" onClick={onToolFilterClick}/>
     </div>
   )
 }
