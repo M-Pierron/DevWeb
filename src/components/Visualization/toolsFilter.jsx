c9d28def9e30cb817f53fd8da18c92e7837d661c
WirbelWind03 <dylan19@gmail.com>
2025-04-11 07:35:17 +0200

    Changes

--- src/components/Visualization/toolsFilter.jsx
+++ src/components/Visualization/toolsFilter.jsx
@@ -2,13 +2,29 @@ import React, { useState, useEffect, useRef } from "react";
 
 import AccordionItem from "../Accordion/accordionItem";
 
-const toolsFilter = ({tools, isVisible, toggleVisibility}) => {
-
+const toolsFilter = ({devicesCategories, isVisible, toggleVisibility}) => {
   return (
     <>
       <div className={`absolute top-0 right-0 p-4 h-full w-full bg-gray-500 overflow-y-auto ${isVisible ? "block" : "hidden"}`}>
         <input type="image" src="/src/assets/backArrow.svg" className="h-[5%] w-full mb-4" onClick={toggleVisibility}/>
-          {tools.map(([categoryName, categoryId, devices]) => (
+          {devicesCategories && devicesCategories.length > 0 && devicesCategories.map((category) => (
+            <AccordionItem 
+              key={category._id}
+              category={category.name}
+              items={category.devices.map((device) => (
+                <div key={device._id}>
+                  <input className='mr-2 bg-white' type="checkbox" name="interest" value={device.name} />
+                  <label htmlFor={device.name}>{device.name}</label>
+                </div>
+              ))}
+              headerStyle={"w-full h-10 flex flex-row bg-gray-300 text-black p-2"}
+              headerOpenStyle={"rounded-t-sm"}
+              headerClosedStyle={"rounded-sm"}
+              blockStyle={"border-3 rounded-b-sm border-gray-300 bg-white p-2 text-black"}
+              headerImage={category.imagePath}
+            />
+          ))}
+          {/* {tools.map(([categoryName, categoryId, devices]) => (
             <AccordionItem 
               key={categoryId} 
               category={categoryName} 
@@ -25,7 +41,7 @@ const toolsFilter = ({tools, isVisible, toggleVisibility}) => {
               blockStyle={"border-3 rounded-b-sm border-gray-300 bg-white p-2 text-black"}
               headerImage={`toolsFilter/${categoryId}.svg`}
             />
-          ))}
+          ))} */}
         <button className="bg-blue-700 w-full">APPLIQUER</button>
       </div>
     </>
