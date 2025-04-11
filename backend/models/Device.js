2cfc870a5ee91b997beb38b32bebf0f2d7815b55
WirbelWind03 <dylan19@gmail.com>
2025-04-11 07:34:31 +0200

    Simplified model

--- backend/models/Device.js
+++ backend/models/Device.js
@@ -1,97 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const WIFI_ENUM = [
-    'NONE', 
-    'WEAK', 
-    'MODERATE', 
-    'STRONG', 
-]
-
-const DEVICE_TYPE_ENUM = [
-    "smart_locks",
-    "video_doorbell",
-    "security_cameras",
-    "motion_sensors",
-    "smart_alarms",
-  
-    "smart_bulbs",
-    "smart_switches",
-    "smart_plugs",
-    "smart_power_strips",
-  
-    "smart_thermostats",
-    "smart_ceiling_fans",
-    "smart_air_purifiers",
-    "smart_humidifiers",
-  
-    "smart_tvs",
-    "smart_speakers",
-    "streaming_devices",
-    "smart_projectors",
-  
-    "smart_fridges",
-    "smart_ovens",
-    "smart_coffee_machines",
-    "smart_dishwashers",
-  
-    "robot_vacuums",
-    "smart_washers",
-    "smart_irrigation"
-];
-
-const DEVICE_CATEGORY_MAP = {
-    smart_locks: "security",
-    video_doorbell: "security",
-    security_cameras: "security",
-    motion_sensors: "security",
-    smart_alarms: "security",
-  
-    smart_bulbs: "energy",
-    smart_switches: "energy",
-    smart_plugs: "energy",
-    smart_power_strips: "energy",
-  
-    smart_thermostats: "comfort",
-    smart_ceiling_fans: "comfort",
-    smart_air_purifiers: "comfort",
-    smart_humidifiers: "comfort",
-  
-    smart_tvs: "entertainment",
-    smart_speakers: "entertainment",
-    streaming_devices: "entertainment",
-    smart_projectors: "entertainment",
-  
-    smart_fridges: "kitchen",
-    smart_ovens: "kitchen",
-    smart_coffee_machines: "kitchen",
-    smart_dishwashers: "kitchen",
-  
-    robot_vacuums: "cleaning",
-    smart_washers: "cleaning",
-    smart_irrigation: "cleaning"
-};
-
-const deviceSchema = new mongoose.Schema({
+// Schema qui réprésente un appareil
+const DeviceSchema = new mongoose.Schema({
+    // Nom de l'appareil
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    // Description de l'appareil, dire ce que l'appareil fait
     name: {
         type: String,
-        required : true
+        required: false,
     },
-    description : String,
-    id: { type: String, required: true, unique: true },
-    mode : {type: String },
-    battery: Number,
-    wifi: { 
-        type: String, 
-        enum: WIFI_ENUM,
-        default: 'NONE'
-    },
-    type : {
+    // La catégorie qui est lié à cette appareil
+    categoryId: {
         type: String,
-        enum: DEVICE_TYPE_ENUM,
-        required: true
-    }
+        ref: 'DeviceCategory',  
+        required: true,
+    },
+    attributes: {
+        type: Map,
+        of: mongoose.Schema.Types.Mixed,
+        required: false,
+    },
 });
 
-const Device = mongoose.model('Device', deviceSchema);
-
-module.exports = Device;
+module.exports = mongoose.model('Device', DeviceSchema);
