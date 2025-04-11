2129157907a8d508ca7cba00f0c5b007cf94df48
WirbelWind03 <dylan19@gmail.com>
2025-04-12 00:31:17 +0200

    Move functions to utils

--- src/components/DashBoard/Widgets/wifi.jsx
+++ src/components/DashBoard/Widgets/wifi.jsx
@@ -1,39 +1,10 @@
 import React from 'react';
+import { getWifiColor, getWifiStrength } from '../../../utils';
 
 const Wifi = ({ strength }) => {
-  const getBars = (level) => {
-    switch (level) {
-      case 'STRONG':
-        return [true, true, true, true];
-      case 'MODERATE':
-        return [true, true, true, false];
-      case 'WEAK':
-        return [true, true, false, false];
-      case 'VERY_WEAK':
-        return [true, false, false, false];
-      default:
-        return [false, false, false, false];
-    }
-  };
-
-  const getColor = (level) => {
-    switch (level) {
-      case 'STRONG':
-        return 'bg-green-500'; // green for strong
-      case 'MODERATE':
-        return 'bg-yellow-400'; // yellow for moderate
-      case 'WEAK':
-        return 'bg-red-500'; // red for weak
-      case 'VERY_WEAK':
-        return 'bg-gray-300'; // gray for very weak
-      default:
-        return 'bg-gray-300'; // default to gray
-    }
-  };
-
-  const bars = getBars(strength);
+  const bars = getWifiStrength(strength);
   const heights = ['25%', '50%', '75%', '100%'];
-  const color = getColor(strength); // Get the color based on strength
+  const color = getWifiColor(strength);
 
   return (
     <div className='flex flex-col items-center justify-center size-full'>
