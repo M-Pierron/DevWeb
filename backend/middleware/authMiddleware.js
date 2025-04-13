17e4002d4eef242c168c50952ffec6a74cd6d0a3
M-Pierron <130355886+M-Pierron@users.noreply.github.com>
2025-04-13 15:01:03 +0200

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,18 +1,16 @@
-// middleware/authMiddleware.js
-
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-    const token = req.headers.authorization && req.headers.authorization.split(" ")[1]; // Extrait le token du header
+    const token = req.headers.authorization && req.headers.authorization.split(" ")[1]; // -- Extrait le token du header --
 
     if (!token) {
         return res.status(401).json({ error: "Token manquant" });
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET); // Décode le token avec la clé secrète
-        req.user = decoded; // Assure-toi que `decoded` contient bien l'ID de l'utilisateur
-        next(); // Passe à la route suivante
+        const decoded = jwt.verify(token, process.env.JWT_SECRET); // -- Décode le token avec la clé secrète --
+        req.user = decoded; 
+        next(); // -- Passe à la route suivante --
     } catch (error) {
         return res.status(401).json({ error: "Token invalide" });
     }
