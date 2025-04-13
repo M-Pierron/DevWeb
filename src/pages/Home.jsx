c56ba2cbad002647c107b124cb43b578dc6c777e
Damien.M <133344364+MouchardDamien@users.noreply.github.com>
2025-04-13 11:06:00 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,43 @@
 import Nav from "../components/nav.jsx"
+import Logo from "../components/logo"
+import { ArrowDown } from 'lucide-react';
 import "../App.css"
 
 function Home() {
   return (
+    <>
+    <Nav name={"ACCUEIL"}/>
+    <div className="home">
     <div className="flex flex-col w-dvw h-dvh">
-      <Nav name={"ACCUEIL"}/>
       
-      <div className="flex flex-row size-full bg-[url(src/assets/wallpaperHome.png)] bg-no-repeat bg-center bg-cover h-[80%] p-4">
+      
+      <div className="flex flex-row size-full bg-[url(src/assets/wallpaperHome.png)] bg-no-repeat bg-center bg-cover h-[85%] p-4">
         <div className="flex flex-col w-full justify-center items-center self-center">
-          <img src="src/assets/logoCYHouse.png" className="size-fit mb-4"></img>
-          <span className="font-bold text-black text-6xl mb-4">CYHOUSE</span>
+          <Logo className="logo_home"/>
+          <span className="font-bold text-white text-6xl mt-65 absolute">CYHOUSE</span>
+            <div className = "arrows">
+              <ArrowDown className="ml-250 mt-145 absolute" size={80}/>
+              <ArrowDown className="mr-250 mt-145 absolute" size={80} />
+            </div>
+            <h3 className="sub_home">Descendez pour plus d'informations.</h3>
+          
         </div>
-        <div className="bg-black w-full">
-          <div className="bg-red-50 w-[40%] h-[20%]"></div>
-        </div>
+       
         
       </div>
 
-      <div className="flex flex-col bg-[#3c5497] w-full h-[20%] border-t-black border-t-2">
-        <p className="self-center text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae dui dolor. Cras congue sed eros vitae accumsan. Donec porttitor interdum urna, et dignissim velit consectetur sit amet. Aenean at dapibus tellus, blandit scelerisque quam. Curabitur vehicula magna in sapien pellentesque, in cursus augue accumsan. Suspendisse vel elit sit amet elit placerat vehicula. Vivamus sit amet nibh ex. Sed quis tempor risus. Sed maximus sagittis risus in ornare. Sed interdum, nulla id tempus suscipit, velit ipsum scelerisque turpis, vitae tincidunt nisi nunc sed est.</p>
+      <div className="flex flex-col bg-[#3c5497] w-full h-[15%] ">
+        <div className="self-center text-center ml-10 mr-10 mt-5"> 
+          <p>Bienvenue sur CY-House !!! Pour plus d'informations concernant le site web, nous vous invitons à consultet la partie "À Propos" présente dans la barre de navigation en haut de la page. </p> 
+          <p>Si vous rencontrez quelquonques problèmes durant votre utilisation de CY-House, contactez notre assistant au 06 95 80 07 46. </p>
+          <p>Bonne continuations sur CY-House !!!</p>
+        </div>
       </div>
     </div>
+    </div>
+    </>
   )
 }
 
 export default Home
+
