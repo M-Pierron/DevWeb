c4f174910f4585922efedbf5f02323bed16a5e28
Damien.M <133344364+MouchardDamien@users.noreply.github.com>
2025-04-13 18:26:37 +0200

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ function About() {
   return (
     <>
     <Nav name ="À PROPOS"/>
-    <div className ="about">
+    <div className ="about mt-10">
     <div className="min-h-screen bg-gray-50">
       <div className="max-w-7xl mx-auto px-4 py-16 sm:px-6 lg:px-8">
         <div className="text-center">
