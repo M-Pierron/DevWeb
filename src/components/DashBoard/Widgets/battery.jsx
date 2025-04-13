646abe7b2d003b901a21d4467e127445ab80bd8c
M-Pierron <130355886+M-Pierron@users.noreply.github.com>
2025-04-13 15:18:17 +0200

    Update battery.jsx

--- src/components/DashBoard/Widgets/battery.jsx
+++ src/components/DashBoard/Widgets/battery.jsx
@@ -4,7 +4,6 @@ const battery = ({percentage}) => {
   const color = percentage > 60 ? 'bg-green-500' : percentage > 30 ? 'bg-yellow-400' : 'bg-red-500';
 
   return (
-    // Battery frame
     <div className="border-2 border-black relative w-full bg-gray-200 rounded-full h-[80%] text-black">
       <div
         className={`${color} h-full rounded-full`}
